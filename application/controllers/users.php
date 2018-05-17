a35c59b277d9292bc79946cd08cfb292f0533eb3
Jim Yoon <jimyoon90@gmail.com>
2018-05-16 18:21:23 -0700

    moved form validations to model, finished registration

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -3,15 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller {
 
-  public function __construct()
-  {
-    // load the models
-    parent::__construct();
-    $this->load->model('user');
-    // need posts, comments, likes etc
-    // $this->load->model('');
-  }
-
   // load the new user registration page
 	public function newreg()
 	{
@@ -21,18 +12,25 @@ class Users extends CI_Controller {
   // register the user
   public function reg()
   {
-    // first validate
-    $this->load->library('form_validation');
-    $this->form_validation->set_rules('first_name', 'first name', 'trim|required');
-    $this->form_validation->set_rules('last_name', 'last name', 'trim|required');
-    $this->form_validation->set_rules('email', 'email', 'trim|required|valid_email|is_unique[users.email]');
-    $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[8]');
-    $this->form_validation->set_rules('password_confirmation', 'password confirmation', 'trim|required|matches[password]');
+    $this->load->model('User');
 
-    // error
-    if($this->form_validation->run())
+    // validate post data
+    $result = $this->User->validate($this->input->post());
+    
+    // if it passes
+    if($result == "valid") 
     {
-      redirect('/users/newreg');
+      $id = $this->User->create($this->input->post());
+      $success[] = "Thank you for signing up! Now please login!";
+      $this->session->set_flashdata('success', $success);
+      redirect('/users/logpage', $success);
+    }
+    // if it fails
+    else 
+    {
+      $errors = array(validation_errors());
+      $this->session->set_flashdata('errors', $errors);
+      redirect('/users/newreg', $errors);
     }
   }
 
---
+++ application/models/user.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Model {
+
+  // find a user
+  public function find($id)
+  {
+    return $this->db->query("SELECT * FROM users WHERE id = ?", array($id)) -> row_array();
+  }
+
+  // create a user
+  public function create($post)
+  {
+    $query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (?,?,?,?,?,?)";
+    $values = array($post['first_name'], $post['last_name'], $post['email'], md5($post['password']), date("Y-m-d, H:i:s"), date("Y-m-d, H:i:s"));
+    $id = $this->db->insert_id($this->db->query($query, $values));
+    return $id;
+  }
+
+  // validate the post data before going into the database
+  public function validate($post) 
+  {
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('first_name', 'first name', 'trim|required');
+    $this->form_validation->set_rules('last_name', 'last name', 'trim|required');
+    $this->form_validation->set_rules('email', 'email', 'trim|required|valid_email|is_unique[users.email]');
+    $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[8]|matches[password_confirmation]');
+    $this->form_validation->set_rules('password_confirmation', 'password confirmation', 'trim|required');
+    if($this->form_validation->run()) 
+    {
+      return "valid";
+    }
+    else 
+    {
+      return array(validation_errors());
+    }
+  }
+
+}
